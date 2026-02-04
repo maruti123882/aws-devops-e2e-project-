546e32e3dbdc83ce220db604ce7ebef5b3ca386a
Maruti Dodamani <maruti.dodamani9972@gmail.com>
2026-02-04 20:18:34 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,13 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class DemoApplication {
+@RestController
+public class HelloController {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+    @GetMapping("/")
+    public String hello() {
+        return "E2E Java App is running 🚀";
     }
 }
-
-
